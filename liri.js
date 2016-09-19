f1e8bc0454efb10910639acaf38f1034c0242646
Kevin Shukla <kevn1841@gmail.com>
2016-09-19 16:08:02 -0400

    tweets added

--- liri.js
+++ liri.js
@@ -19,7 +19,17 @@ var fifthArg = process.argv[6];
 var sixthArg = process.argv[7];
 
 if(firstArg == "my-tweets"){
-
+	var client = new Twitter(keys.twitterKeys);
+	var params = {screen_name: 'inrtracker'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+	  if (!error) {
+	  	for(var i=0; i < tweets.length; i++){
+	  		console.log(tweets[i].created_at);
+	  		console.log('');
+	  		console.log(tweets[i].text);
+	  	}
+	  }
+	});	
 }else if(firstArg == "spotify-this-song"){
 
 }else if(firstArg == "movie-this"){
